15039044de146c6c0f55b8c7818064520024611c
Vadim Yelagin <vadim@fueled.com>
2016-06-30 23:24:58 +0600

    Add empty project

---
+++ FueledUtils/FueledUtils.h
@@ -0,0 +1,19 @@
+//
+//  FueledUtils.h
+//  FueledUtils
+//
+//  Created by Vadim Yelagin on 30/06/16.
+//  Copyright © 2016 Fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FueledUtils.
+FOUNDATION_EXPORT double FueledUtilsVersionNumber;
+
+//! Project version string for FueledUtils.
+FOUNDATION_EXPORT const unsigned char FueledUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FueledUtils/PublicHeader.h>
+
+
