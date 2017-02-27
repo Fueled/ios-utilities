4609a7af1dcd0d95f15cedc1d32c27738e0f2a43
Vadim Yelagin <vadim.yelagin@gmail.com>
2017-02-27 20:44:51 +0700

    Add ReactiveLifetimeProvider

---
+++ FueledUtils/ReactiveLifetimeProvider.swift
@@ -0,0 +1,24 @@
+import ReactiveSwift
+
+public protocol ReactiveLifetimeProvider: ReactiveExtensionsProvider {
+	var lifetimeToken: Lifetime.Token { get }
+}
+
+open class Lifetimed: ReactiveLifetimeProvider {
+	public let lifetimeToken = Lifetime.Token()
+	public init() {
+	}
+}
+
+public extension Reactive where Base: ReactiveLifetimeProvider {
+	public var lifetime: Lifetime {
+		return Lifetime(self.base.lifetimeToken)
+	}
+	public func makeBindingTarget<U>(on scheduler: SchedulerProtocol = UIScheduler(), _ action: @escaping (Base, U) -> Void) -> BindingTarget<U> {
+		return BindingTarget(on: scheduler, lifetime: lifetime) { [weak base = self.base] value in
+			if let base = base {
+				action(base, value)
+			}
+		}
+	}
+}
