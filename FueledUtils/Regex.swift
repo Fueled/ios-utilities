b19b798c1ffc44b12461e8ca0f8b30116c2f99f0
Vadim Yelagin <vadim@fueled.com>
2017-03-20 22:12:37 +0700

    Make Regex public

--- FueledUtils/Regex.swift
+++ FueledUtils/Regex.swift
@@ -3,11 +3,11 @@ import Foundation
 public struct Regex {
 	fileprivate let impl: NSRegularExpression
 
-	init(_ pattern: String, options: NSRegularExpression.Options = []) {
+	public init(_ pattern: String, options: NSRegularExpression.Options = []) {
 		impl = try! NSRegularExpression(pattern: pattern, options: options)
 	}
 
-	func match(_ string: String, options: NSRegularExpression.MatchingOptions = []) -> Bool {
+	public func match(_ string: String, options: NSRegularExpression.MatchingOptions = []) -> Bool {
 		return impl.numberOfMatches(in: string, options: options, range: string.fullRange) != 0
 	}
 }
