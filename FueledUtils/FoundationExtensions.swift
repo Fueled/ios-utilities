66b4adeccb4365eeeda6bd28609d7253926ffa5d
Leonty Deriglazov <leontyd@me.com>
2017-08-29 16:51:02 +0100

    feat(bool): add flip function

---
+++ FueledUtils/FoundationExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  FoundationExtensions.swift
+//  FueledUtils
+//
+//  Created by Leonty Deriglazov on 29/08/2017.
+//  Copyright © 2017 Fueled. All rights reserved.
+//
+
+import Foundation
+
+public extension Bool {
+	public mutating func flip() {
+		self = !self
+	}
+}
