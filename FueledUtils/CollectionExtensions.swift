874e7c608d0a153548989c004578a34dca0fb0a0
Stéphane Copin <stephane@fueled.com>
2017-09-04 16:01:42 +0100

    chore(comments): fix and implement PR comments and update reactive swift/cocoa

--- FueledUtils/CollectionExtensions.swift
+++ FueledUtils/CollectionExtensions.swift
@@ -1,5 +1,5 @@
 public extension Collection {
-	public func get(_ index: Self.Index) -> Self.Iterator.Element? {
+	public func getSafely(_ index: Self.Index) -> Self.Iterator.Element? {
 		if index < self.startIndex || index > self.endIndex {
 			return nil
 		}
