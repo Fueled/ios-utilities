a39e801e2b22c8cc2ffdb22dd3d705aa236e53ea
Stéphane Copin <stephane@fueled.com>
2020-04-13 14:43:20 -0400

    feat(swift-ui/for-each-with-index): replace the existing helper for creating a collection with indices with ForEachWithIndex

---
+++ FueledUtils/SwiftUI/ForEachWithIndex.swift
@@ -0,0 +1,60 @@
+//
+//  ForEachWithIndex.swift
+//  RPMHelpers
+//
+//  Created by Stéphane Copin on 4/10/20.
+//  Copyright © 2020 Fueled. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ForEachWithIndex<Data: RandomAccessCollection, ID: Hashable, Content: View>: View {
+	var data: Data
+	var id: KeyPath<Data.Element, ID>
+	var content: (_ index: Data.Index, _ element: Data.Element) -> Content
+
+	public init(_ data: Data, id: KeyPath<Data.Element, ID>, content: @escaping (_ index: Data.Index, _ element: Data.Element) -> Content) {
+		self.data = data
+		self.id = id
+		self.content = content
+	}
+
+	public var body: some View {
+		ForEach(
+			zip(self.data.indices, self.data).map { index, element in
+				IndexInfo(
+					index: index,
+					id: self.id,
+					element: element
+				)
+			},
+			id: \.elementID
+		) { indexInfo in
+			self.content(indexInfo.index, indexInfo.element)
+		}
+	}
+}
+
+extension ForEachWithIndex where ID == Data.Element.ID, Content: View, Data.Element: Identifiable {
+	public init(_ data: Data, @ViewBuilder content: @escaping (_ index: Data.Index, _ element: Data.Element) -> Content) {
+		self.init(data, id: \.id, content: content)
+	}
+}
+
+private struct IndexInfo<Index, Element, ID: Hashable>: Hashable {
+	let index: Index
+	let id: KeyPath<Element, ID>
+	let element: Element
+
+	var elementID: ID {
+		self.element[keyPath: self.id]
+	}
+
+	static func == (_ lhs: IndexInfo, _ rhs: IndexInfo) -> Bool {
+		lhs.elementID == rhs.elementID
+	}
+
+	func hash(into hasher: inout Hasher) {
+		self.elementID.hash(into: &hasher)
+	}
+}
