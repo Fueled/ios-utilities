f70301b6e04bce57940d8f46c770cbeab916c81b
Stéphane Copin <stephane@fueled.com>
2020-05-13 13:11:26 -0400

    chore(blur): move BlurView to its own file

--- FueledUtils/SwiftUI/BlurView.swift
+++ FueledUtils/SwiftUI/BlurView.swift
@@ -14,13 +14,14 @@
 
 import SwiftUI
 
-extension View {
-	public func backgroundBlur(style: UIBlurEffect.Style, color: Color? = nil) -> some View {
-		ZStack {
-			color
-			BlurView(style: style)
-			self
-				.eraseToAnyView()
-		}
+public struct BlurView: UIViewRepresentable {
+	public let style: UIBlurEffect.Style
+
+	public func makeUIView(context: Context) -> UIVisualEffectView {
+		UIVisualEffectView(effect: UIBlurEffect(style: self.style))
+	}
+
+	public func updateUIView(_ visualEffectView: UIVisualEffectView, context: Context) {
+		visualEffectView.effect = UIBlurEffect(style: self.style)
 	}
 }
